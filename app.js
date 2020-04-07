a750de0180d0d29c5c88014b7274bb1eefd4bac9
d-d-wolfe <dave.wolfe623@gmail.com>
2020-04-07 08:48:28 -0700

    Added function to calculate cookies per hour.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
 
+//create an object called seattleStore to hold min and max cust per hour
+//average cookie sales, and hours open
+
+//create a function to calculate a random number of customers per hour
+
 var seattleStore = {
   minCust : 23,
   maxCust : 65,
   avgCookieSale : 6.3,
   custPerHour : [],
+  cookiesPerHour : [],
   hoursOpen : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
 }
 
@@ -14,9 +20,22 @@ seattleStore.calcCustPerHour = function (){
    var randomCustomer = Math.round(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   this.custPerHour.push(randomCustomer);
   }
+//create a function to calculate the cookies needed per hour, based on customers per hour and average cookie sales per customer.
+  for (var i = 0; i < this.custPerHour.length; i++) {
+    var calcCookiesPerHour = Math.round(this.custPerHour[i] * this.avgCookieSale)
+    console.log(calcCookiesPerHour)
+  }
+
 }
 seattleStore.calcCustPerHour();
 console.log(seattleStore.custPerHour);
+
+
+// create a table to display the hours open and the random customers per hour, and how many cookies are needed per hour
+
+
+
+
 
 // var tokyoStore = {
 //   tokMinCust : 3,
