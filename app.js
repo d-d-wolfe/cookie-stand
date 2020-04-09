239544f71d1735cd87b57a8e12ff43b83e82de4d
d-d-wolfe <dave.wolfe623@gmail.com>
2020-04-08 20:14:59 -0700

    made some changes to the js, and started to code the CSS.

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
 //create an object called seattleStore to hold min and max cust per hour
 //average cookie sales, and hours open
 
+//create object constructors
+
 var seattleStore = {
   minCust : 23,
   maxCust : 65,
   avgCookieSale : 6.3,
   custPerHour : [],
   cookiesPerHour : [],
+  dailyTotal : 0,
   hoursOpen : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  
 }
 
 //create a function to calculate a random number of customers per hour
@@ -17,7 +21,7 @@ var seattleStore = {
 seattleStore.calcCustPerHour = function (){
  
   for (var i = 0; i < this.hoursOpen.length; i++) {
-   var randomCustomer = Math.round(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+   var randomCustomer = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
    this.custPerHour.push(randomCustomer);
   }
 }
@@ -27,34 +31,51 @@ seattleStore.calcCustPerHour = function (){
 seattleStore.calcCookiesPerHour = function() {
 
   for (var i = 0; i < this.custPerHour.length; i++) {
-    var calcCookiesPerHour = Math.round(this.custPerHour[i] * this.avgCookieSale)
-    this.cookiesPerHour.push(calcCookiesPerHour)
+    var cookiesPH = Math.floor(this.custPerHour[i] * this.avgCookieSale)
+    this.cookiesPerHour.push(cookiesPH)
     //console.log(calcCookiesPerHour)
   }
 }
+
+seattleStore.totalSalesPerHour = function() {
+  this.calcCookiesPerHour();
+  var tot = 0;
+  for (var i = 0; i < this.cookiesPerHour.length; i++) {
+  tot = tot + this.cookiesPerHour[i];
+  console.log(tot);
+  
+  //this.dailyTotal += totalSales;
+  //console.log(totalSales);
+  }
+  return tot
+ }
+
+console.log(seattleStore.cookiesPerHour)
 seattleStore.calcCustPerHour();
-seattleStore.calcCookiesPerHour();
-console.log(seattleStore.custPerHour);
-console.log(seattleStore.cookiesPerHour);
+seattleStore.totalSalesPerHour();
+// console.log(seattleStore.custPerHour);
+// console.log(seattleStore.cookiesPerHour);
+//console.log()(seattleStore.totalSalesPerHour);
 
 seattleStore.renderToPage = function () {
-  var targetUlEl = document.getElementById('seattle');
+  var div = document.getElementById('seattle')
+  var newUlEl = document.createElement('ul');
+  var storeEl = document.createElement('h1')
   var newLiEl = document.createElement('li');
-  var custLiEl = document.createElement('li');
-  var listText = this.cookiesPerHour;
-  var custListText = this.custPerHour;
+  var listText = this.cookiesPerHour[0];
+  var hoursList = this.hoursOpen[0];
   
-  newLiEl.textContent = 'Cookies Per hour ' + listText;
-  custLiEl.textContent = 'Customers per hour ' + custListText;
+  newLiEl.textContent = hoursList + ' ' + listText;
+ 
 
-  targetUlEl.appendChild(custLiEl);
-  targetUlEl.appendChild(newLiEl);
+  //targetUlEl.appendChild(newLiEl);
  
   //console.log(newLiEl)
 }
 seattleStore.renderToPage();
 
-// create a table to display the hours open and the random customers per hour, and how many cookies are needed per hour
+// need a total - add up the total from each hour (math)
+// create a table to display the hours open and the random customers per hour, and how many cookies are needed per hour - find the target - create content for the target (labels, numbers [in rows], text in cells[th and td]) - append the info
 
 
 var tokyoStore = {
@@ -69,7 +90,7 @@ var tokyoStore = {
 //tokyoStore.calcTokCustPerHour = function() {
 tokyoStore.c = function() {
   for (var i = 0; i < this.tokHoursOpen.length; i++) {
-    var tokRandomCust = Math.round(Math.random() * (this.tokMaxCust - this.tokMinCust + 1) + this.tokMinCust);
+    var tokRandomCust = Math.floor(Math.random() * (this.tokMaxCust - this.tokMinCust + 1) + this.tokMinCust);
     this.tokCustPerHour.push(tokRandomCust);
     
     }
@@ -78,7 +99,7 @@ tokyoStore.c = function() {
 tokyoStore.tokCalcCookiesPerHour = function() {
 
   for (var i = 0; i < this.tokCustPerHour.length; i++) {
-    var tokCalcCookiesPerHour = Math.round(this.tokCustPerHour[i] * this.tokAvgCookieSale)
+    var tokCalcCookiesPerHour = Math.floor(this.tokCustPerHour[i] * this.tokAvgCookieSale)
     this.tokCookiesPerHour.push(tokCalcCookiesPerHour)
     // console.log(tokCalcCookiesPerHour)
     
@@ -87,8 +108,8 @@ tokyoStore.tokCalcCookiesPerHour = function() {
 }
 tokyoStore.c();
 tokyoStore.tokCalcCookiesPerHour();
-console.log(tokyoStore.c);
-console.log(tokyoStore.tokCalcCookiesPerHour);
+// console.log(tokyoStore.c);
+// console.log(tokyoStore.tokCalcCookiesPerHour);
 
 tokyoStore.renderToPage = function () {
   var targetUlEl = document.getElementById('tokyo');
@@ -100,8 +121,8 @@ tokyoStore.renderToPage = function () {
   newLiEl.textContent = 'Cookies per hour ' + listText;
   tokCustLiEl.textContent = 'Customers per hour ' + tokCustListText;
 
-  targetUlEl.appendChild(tokCustLiEl);
-  targetUlEl.appendChild(newLiEl);
+  //targetUlEl.appendChild(tokCustLiEl);
+  //targetUlEl.appendChild(newLiEl);
 }
 tokyoStore.renderToPage();
 
