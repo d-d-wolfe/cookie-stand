3866c672f4b52dfcac065768e050a2eceded2b13
d-d-wolfe <dave.wolfe623@gmail.com>
2020-04-12 13:16:46 -0700

    Completed the form in html and revamped the js to allow the form to function correctly.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 
- var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-//create object constructor
+var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var locationObject = [];
+var tableStoreRow = document.getElementById('content');
+var tableFootRow = document.getElementById('footrow');
+var newStoreForm = document.getElementById('form');
 
-function Store(minCust, maxCust, avgCookieSale) {
+//create object constructor
+function Store(location, minCust, maxCust, avgCookieSale) {
+  this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
@@ -43,45 +48,19 @@ Store.prototype.totalSalesPerHour = function() {
   this.dailyTotal = tot;
  }
 
-//this method calculates the hourly total
+//this function calculates the hourly total
 
 var createHourlyTotal = function(stores) {
   var totalHourlySalesArr = [];
   for (var i = 0; i < hoursOpen.length; i++) {
     var hourlyTotal = 0;
-    hourlyTotal = stores[0].cookiesPerHour[i] + stores[1].cookiesPerHour[i] + stores[2].cookiesPerHour[i] + stores[3].cookiesPerHour[i] + stores[4].cookiesPerHour[i];
+    for (var j = 0; j < stores.length; j++) {
+      hourlyTotal = hourlyTotal + stores[j].cookiesPerHour[i];
+    }
     totalHourlySalesArr.push(hourlyTotal);
   }
   return totalHourlySalesArr;
 }
-
-// calling each of the stores and methods
-var seattle = new Store(23,65,6.3);
-seattle.calcCustPerHour();
-seattle.calcCookiesPerHour();
-seattle.totalSalesPerHour();
-//console.log('Seattle constructor ', seattle)
-var tokyo = new Store(3, 24, 1.2);
-tokyo.calcCustPerHour();
-tokyo.calcCookiesPerHour();
-tokyo.totalSalesPerHour();
-var paris = new Store(20, 38, 2.3);
-paris.calcCustPerHour();
-paris.calcCookiesPerHour();
-paris.totalSalesPerHour();
-var dubai = new Store(11, 38, 3.7);
-dubai.calcCustPerHour();
-dubai.calcCookiesPerHour();
-dubai.totalSalesPerHour();
-var lima = new Store(2, 16, 4.6);
-lima.calcCustPerHour();
-lima.calcCookiesPerHour();
-lima.totalSalesPerHour();
-var stores = [seattle, tokyo, paris, dubai, lima];
-var hourlySalesArr = createHourlyTotal(stores);
-//console.log('Hourly sales Total ', hourlySalesArr);
-
-// method to create the header row of the table
 
 var createHeader = function () {
   var headRow = '<td>' + 'Location' + '</td>';
@@ -95,27 +74,26 @@ var createHeader = function () {
   tableHeadRow.innerHTML = headRow;
 
 }
-createHeader();
 
-// method to create each of the stor rows in the table
+// function to create each of the stor rows in the table
 
-var createStoreRow = function (storeName, storeObject) {
+var createStoreRow = function (storeObject) {
+  
   //console.log(storeObject)
-  var storeRow = '<td>' + storeName + '</td>';
+  var storeRow = '<td>' + storeObject.location + '</td>';
   //console.log(storeObject.cookiesPerHour);
   for (var i = 0; i < storeObject.cookiesPerHour.length; i++) {
     storeRow = storeRow + '<td>' + storeObject.cookiesPerHour[i] + '</td>';
   }
   storeRow = storeRow + '<td>' + storeObject.dailyTotal + '</td>';
   //console.log('Store row ' , storeRow)
-  var tableStoreRow = document.getElementById('content');
   var newRow = document.createElement('tr');
   newRow.innerHTML = storeRow;
   tableStoreRow.appendChild(newRow);
 
 }
 
-//method to create the totals row of the table
+//function to create the totals row of the table
 
 var createFooter = function (hourlySales) {
   var footRow = '<td>' + 'Totals' + '</td>';
@@ -125,18 +103,72 @@ var createFooter = function (hourlySales) {
     grandTotal = grandTotal + hourlySales[i];
   }
   footRow = footRow + '<td>' + grandTotal + '</td>';
-  var tableFootRow = document.getElementById('footrow');
   //console.log('Teble head row in createHeader ',tableFootRow);
   tableFootRow.innerHTML = footRow;
 
 }
 
-createStoreRow('Seattle', seattle);
-createStoreRow('Tokyo', tokyo);
-createStoreRow('Dubai', dubai);
-createStoreRow('Paris', paris);
-createStoreRow('Lima', lima);
-createFooter(hourlySalesArr);
+function createTable() {
+  createHeader();
+
+  for (var i = 0; i < locationObject.length; i++) {
+    createStoreRow(locationObject[i]);
+  }
+  var x = createHourlyTotal(locationObject);
+  createFooter(x);
+}
+
+//console.log('i\'m in form details ', locationObject)
+function formDetails(event) {
+  event.preventDefault();
+  var location = event.target.location.value;
+  var minCust = event.target.mincust.value;
+  var maxCust = event.target.maxcust.value;
+  var avgCookieSale = event.target.avg.value;
+  var newStore = new Store(location, minCust, maxCust, avgCookieSale);
+  newStore.calcCustPerHour();
+  newStore.calcCookiesPerHour();
+  newStore.totalSalesPerHour();
+  locationObject.push(newStore);
+  newStoreForm.reset();
+  tableStoreRow.innerHTML = '';
+  tableStoreRow.innerHTML = '';
+  createTable();
+  
+}
+
+function initializeStores() {
+  var seattle = new Store('Seattle', 23,65,6.3);
+  seattle.calcCustPerHour();
+  seattle.calcCookiesPerHour();
+  seattle.totalSalesPerHour();
+  locationObject.push(seattle);
+  //console.log('Seattle constructor ', seattle)
+  var tokyo = new Store('Tokyo',3, 24, 1.2);
+  tokyo.calcCustPerHour();
+  tokyo.calcCookiesPerHour();
+  tokyo.totalSalesPerHour();
+  locationObject.push(tokyo)
+  var paris = new Store('Paris', 20, 38, 2.3);
+  paris.calcCustPerHour();
+  paris.calcCookiesPerHour();
+  paris.totalSalesPerHour();
+  locationObject.push(paris);
+  var dubai = new Store('Dubai', 11, 38, 3.7);
+  dubai.calcCustPerHour();
+  dubai.calcCookiesPerHour();
+  dubai.totalSalesPerHour();
+  locationObject.push(dubai);
+  var lima = new Store('Lima', 2, 16, 4.6);
+  lima.calcCustPerHour();
+  lima.calcCookiesPerHour();
+  lima.totalSalesPerHour();
+  locationObject.push(lima);
+  
+  createTable();
+  
+}
 
 
-
+newStoreForm.addEventListener('submit', formDetails);
+initializeStores();
